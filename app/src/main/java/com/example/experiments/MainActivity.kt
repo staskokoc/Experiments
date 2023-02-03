2fac78ba31b6a66d82f063dec390435056a5ced7
тАЬstaskokocтАЭ <staskokoc@gmail.com>
2023-02-03 11:11:38 +0200

    second_commit

--- app/src/main/java/com/example/experiments/MainActivity.kt
+++ app/src/main/java/com/example/experiments/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.experiments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
